4ec04be6af7b186ec4b9434479c3f74b715a0767
mariogerardi <mgerardi@vassar.edu>
2022-03-02 11:35:07 -0500

    Reconfigured app for CHEESE.

--- server.js
+++ server.js
@@ -19,59 +19,52 @@ mongoose.connection
     .on("close", () => console.log("Your are disconnected from mongoose"))
     .on("error", (error) => console.log(error));
 
-const PeopleSchema = new mongoose.Schema({
+const CheeseSchema = new mongoose.Schema({
     name: String,
     image: String,
     title: String,
 });
   
-const People = mongoose.model("People", PeopleSchema);
+const Cheese = mongoose.model("Cheese", CheeseSchema);
   
-app.use(cors()); // to prevent cors errors, open access to all origins
-app.use(morgan("dev")); // logging
-app.use(express.json()); // parse json bodies
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
   
 app.get("/", (req, res) => {
     res.send("hello world");
 });
   
-app.get("/people", async (req, res) => {
+app.get("/cheese", async (req, res) => {
     try {
-        res.json(await People.find({}));
+        res.json(await Cheese.find({}));
     } catch (error) {
         res.status(400).json(error);
-  }
+    }
 });
   
-app.post("/people", async (req, res) => {
+app.post("/cheese", async (req, res) => {
     try {
-        res.json(await People.create(req.body));
+        res.json(await Cheese.create(req.body));
     } catch (error) {
         res.status(400).json(error);
     }
 });
 
-app.put("/people/:id", async (req, res) => {
+app.put("/cheese/:id", async (req, res) => {
     try {
-      // send all people
-      res.json(
-        await People.findByIdAndUpdate(req.params.id, req.body, { new: true })
-      );
+        res.json(await Cheese.findByIdAndUpdate(req.params.id, req.body, { new: true }));
     } catch (error) {
-      //send error
-      res.status(400).json(error);
+        res.status(400).json(error);
     }
-  });
+});
   
-  // PEOPLE DELETE ROUTE
-  app.delete("/people/:id", async (req, res) => {
+app.delete("/cheese/:id", async (req, res) => {
     try {
-      // send all people
-      res.json(await People.findByIdAndRemove(req.params.id));
+      res.json(await Cheese.findByIdAndRemove(req.params.id));
     } catch (error) {
-      //send error
       res.status(400).json(error);
     }
-  });
+});
 
 app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
